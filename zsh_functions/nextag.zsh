266791c37413592e65e4b3e85ed3d3fb2dd54d75
Samir Faci <samir.faci@nextag.com>
2011-12-16 12:09:07 -0600

    added nextag.zsh to repo.

---
+++ zsh_functions/nextag.zsh
@@ -0,0 +1,5 @@
+PATH=$PATH:$HOME/local/eclipse/
+PATH=$PATH:/Developer/maven3/bin
+export LD_LIBRARY_PATH=/usr/lib64:/usr/lib/jni/ 
+export JAVA_HOME=/usr/lib/jvm/java-6-sun/
+export CATALINA_HOME=/Developer/tomcat
