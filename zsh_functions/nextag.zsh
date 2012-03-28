b296a603ec2fb107080ef4be8481485f93e2e5d1
Samir Faci <samir.faci@nextag.com>
2012-03-28 18:11:26 -0500

    saving changes

--- zsh_functions/nextag.zsh
+++ zsh_functions/nextag.zsh
@@ -14,21 +14,32 @@ alias roottunnel="sudo ssh -L 80:localhost:8080 cali"
 
 
 
-#if (( $+BRNACH )); then 
-#    echo "BRANCH: $BRANCH"
-#else
-#    #BRANCH='BR_NTR_15_APR'
-#    echo "BRANCH not set: $BRANCH"
-#fi
+function setCache()   ## XCache Branch
+{
+    BRANCH='BR_CH_1109'
+    generateAliases
+}
+
+function setDefault()   ##git repo
+{
+    BRANCH='nextag_app'
+    generateAliases
+}
+
+function setIM()  ## backend branch
+{
+    BRANCH='BR_TR_14_IM_AUG'
+    generateAliases
+}
 
 
-function setClassic()
+function setClassic()  #Classic Travel
 {
     BRANCH='BR_TR_15_APR'
     generateAliases
 }
 
-function setNew()
+function setNew()  ## Nextravel branch
 {
     BRANCH='BR_NTR_15_APR'
     generateAliases
@@ -42,6 +53,8 @@ function generateAliases()
     alias work="cd  /home/sfaci/work/$BRANCH/app/main"
     alias classic="cd  /home/sfaci/work/BR_TR_15_APR/app/main"
     alias edit="vim -p java/com/nextag/travel/component/hotel/catalog/impl/lucene/LegacyLuceneHotelSearchAdapter.java  java/com/nextag/travel/component/hotel/catalog/impl/lucene/LegacyLuceneHotelCatalogComponent.java  java/com/nextag/travel/component/hotel/catalog/impl/lucene/LegacyLuceneHotelQueryFactory.java"
+    alias cassar='cd /cacheDir/xcache/prod/apache-cassandra-1.0.7/bin; for i in $(seq 1 8); do echo cass$i; ./nodetool -h cass$i  cfstats | perl -00 -ne "/Tag/ && print"| grep "Read Latency"; done; for i in `seq 1 4`; do echo dcass$i; ./nodetool -h dcass$i  cfstats | perl -00 -ne "/Tag/ && print"| grep "Read Latency"; done; for i in $(seq 1 4); do echo sdcass$i; ./nodetool -h sdcass$i  cfstats | perl -00 -ne "/Tag/ && print"| grep "Read Latency"; done;'
 }
 
+setDefault
 generateAliases
