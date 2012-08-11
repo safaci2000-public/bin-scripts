7706e26fd26243b72397d036d59104c0e3b99219
Samir Faci <dev@esamir.com>
2012-08-11 16:33:32 -0700

    cleaned up aliases and added nodeCalc util

--- zsh_functions/aliases.zsh
+++ zsh_functions/aliases.zsh
@@ -7,8 +7,8 @@ alias techie="ssh user@techieworld.org"
 
 export PATH=~/bin:$PATH
 export PATH=~/bin/git:$PATH  ##adding custom git scripts.
-export PATH=~/bin/git:$PATH  ##adding custom kvm/qemu scripts.
-export PATH=$PATH:/home/user/bin
+export PATH=$PATH:$HOME/bin
+export PATH=$PATH:$HOME/bin/bigdata  ##Adding path for big data scripts
 
 ##Required for silly Ubuntu behavior.
 export PATH=$PATH:/var/lib/gems/1.8/gems/cheat-1.2.1/bin/
@@ -21,16 +21,8 @@ export GDK_NATIVE_WINDOWS=true
 export EDITOR=/usr/bin/vim
 export PATH=$PATH:/usr/local/bin
 
-export PATH=$PATH:$HOME/bin
 export PATH=$PATH:$HOME/projects/android-sdk-linux_86/tools
 
-## CVS
-export CVS_RSH=ssh
-export CVSROOT=cvs:/cvs
-export CVS_RSH CVSROOT
-
-##java
-#
 #alias aptd='sudo apt-get update && sudo apt-get dist-upgrade'  ##debian
 alias apta='sudo pacman -Syu && sudo rm -frv /var/cache/pacman/pkg/*.{xz,gz}'  ## am I really this lazy?  ## arch
 alias aptg='sudo emerge --sync && sudo emerge -udN world -av'  ##gentoo
