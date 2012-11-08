097e5af92e6dd15d002b74ac6fdd09937a68f63f
Samir Faci <samir.faci@wizecommerce.com>
2012-11-08 15:46:43 -0800

    fixing maven autocompletion

--- zsh_functions/maven.zsh
+++ zsh_functions/maven.zsh
@@ -1,17 +1,4 @@
 ##maven 
-maven_plugins=(
-  'jboss'
-  'tomcat'                                       # Simple plugin names can be given...
-  'gwt:Maven plugin for the Google Web Toolkit'  # or take the "name:description" form.
-  'android:Maven Plugin for Android'
-)
-zstyle ':completion:*:mvn:*' plugins $maven_plugins
-#zstyle ':completion:*:mvn:*' show-advanced-phases true
-##make pretty
-zstyle ':completion:*:*:mvn:*:matches' group 'yes'
-zstyle ':completion:*:*:mvn:*:options' description 'yes'
-zstyle ':completion:*:*:mvn:*:options' auto-description '%d'
-zstyle ':completion:*:*:mvn:*:descriptions' format $'\e[1m -- %d --\e[22m'
-zstyle ':completion:*:*:mvn:*:messages' format $'\e[1m -- %d --\e[22m'
-zstyle ':completion:*:*:mvn:*:warnings' format $'\e[1m -- No matches found --\e[22m'
 
+function listMavenCompletions { reply=(cli:execute cli:execute-phase archetype:generate compile clean install test test-compile deploy package cobertura:cobertura jetty:run -Dmaven.test.skip=true -DarchetypeCatalog=http://tapestry.formos.com/maven-snapshot-repository -Dtest= `if [ -d ./src ] ; then find ./src -type f | grep -v svn | sed 's?.*/\([^/]*\)\..*?-Dtest=\1?' ; fi`); }
+compctl -K listMavenCompletions mvn
