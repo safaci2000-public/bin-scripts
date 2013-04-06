55c5a5ee416da5be9b140b88212269d0ead7e935
Samir Faci <samir.faci@wizecommerce.com>
2013-04-05 17:05:39 -0700

    alpha attempt at inhouse zsh auto-completion

---
+++ zsh_functions/wize_custom.zsh
@@ -0,0 +1,5 @@
+##Custom Programs Autocomplete
+
+function listgenerateTriggerCompletions { reply=(--help --table --keys --output  --fields --template --output  ); }
+
+compctl -K listgenerateTriggerCompletions generateTrigger.py
