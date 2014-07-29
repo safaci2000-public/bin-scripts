cf461f9b3f1eb648c39ef9c338bab14411d49248
Samir Faci <samir.faci@smule.com>
2014-07-28 20:54:00 -0700

    Cleanup wize code.

--- zsh_functions/smule.zsh
+++ zsh_functions/smule.zsh
@@ -2,10 +2,22 @@ export PATH=$PATH:/usr/local/mysql/bin
 export PATH=$PATH:$HOME/dev/sfw/play
 export PATH=$PATH:$HOME/dev/sfw/ant/bin/
 
+function showFiles() 
+{
+    defaults write com.apple.finder AppleShowAllFiles YES
+    killall Finder /System/Library/CoreServices/Finder.app
+}
+
+function hideFiles()
+{
+    defaults write com.apple.finder AppleShowAllFiles NO
+    killall Finder /System/Library/CoreServices/Finder.app
+}
+
 function generateAliases()
 {
     alias work='cd ~/dev/git/snp'
-    alias play='cd ~/dev/git/snp/play'
+    alias cdplay='cd ~/dev/git/snp/play'
     alias smysql='sudo /Library/StartupItems/MySQLCOM/MySQLCOM restart'
     alias pushmaster='git push gerrit HEAD:refs/for/master'
     alias rebasem='git rebase -i origin/master'
