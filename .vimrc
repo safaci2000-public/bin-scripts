683648b4dae45772798a8f65ce52d7d53e74df23
Samir Faci <samir@esamir.com>
2017-03-05 00:12:43 -0800

    Converting to zplug + vim-plug

--- .vimrc
+++ .vimrc
@@ -1,39 +1,118 @@
 set nocompatible               " be iMproved
-filetype off                   " required!
+filetype on                    " required!
 
-set rtp+=~/.vim/bundle/vundle/
-call vundle#rc()
+au! BufNewFile,BufRead *.thrift set filetype=thrift
 
-" let Vundle manage Vundle
-" required! 
-Bundle 'gmarik/vundle'
+call plug#begin('~/.vim/plugged')
 
-" My Bundles here:
-"
 
-" original repos on github
-Bundle 'safaci2000-stableProjects/csgeek-vimcustomizations'
-Bundle 'sjl/gundo.vim'
-Bundle 'tpope/vim-fugitive'
-Bundle 'tpope/vim-rails'
-Bundle 'rstacruz/sparkup', {'rtp': 'vim/'}
-""non github
-Bundle 'git://vim-latex.git.sourceforge.net/gitroot/vim-latex/vim-latex'
+""""set rtp+=~/.vim/bundle/vundle/
+""""call vundle#rc()
+""""
+""""" let Vundle manage Vundle
+""""" required! 
+Plug 'tpope/vim-sensible'
+Plug 'safaci2000-stableProjects/csgeek-vimcustomizations'
+"Plug 'safaci2000-stableProjects/csgeek-python.vim.git'
+Plug 'sjl/gundo.vim'
+
+"git stuff
+Plug 'tpope/vim-fugitive'
+"html helper
+Plug 'rstacruz/sparkup', {'rtp': 'vim/'}
+
+Plug 'mbbill/undotree'
+Plug 'LaTeX-Box-Team/LaTeX-Box'
+Plug 'reinh/vim-makegreen'
+"search current work space.
+Plug 'kien/ctrlp.vim'
+"replaces ack.vim, requires: ag ( sudo apt-get install silversearcher-ag)
+Plug 'vim-scripts/ag.vim'
+Plug 'scrooloose/nerdtree'
+Plug 'Townk/vim-autoclose'
+"code Comments
+Plug 'The-NERD-Commenter'
+Plug 'ervandew/supertab'
+" vim environment
+Plug 'bling/vim-airline'
+" colors
+Plug 'altercation/vim-colors-solarized'
+Plug 'spf13/vim-colors'
+Plug 'lsdr/monokai'
+Plug 'mitsuhiko/fruity-vim-colorscheme'
+Plug 'tpope/vim-vividchalk'
+Plug 'wesgibbs/vim-irblack'
+"" vim-scripts
+Plug 'msanders/snipmate.vim'
+"Plug 'neocomplcache'
+"" Vim improvements
+Plug 'tsaleh/vim-align'
+Plug 'Lokaltog/vim-easymotion'
+
+"Note taking 
+Plug 'xolox/vim-notes'
+Plug 'xolox/vim-misc'
+Plug 'utl.vim'
+Plug 'VOoM'
+Plug 'netrw.vim'
+
+
+
+call plug#end()
+
+ 
+
+
+"Possibly useful, but disabled for now.
 
 filetype plugin indent on     " required!
 
 
 syntax on
 set guifont=Consolas\ 11
-colors fruity
+colors vividchalk
 
+
+function! FixCamel()
+    :%s/\([A-Z]\)/\_\1/g
+	:exe "normal \1Gv<Shift>G$U"
+endfunction
 
 cmap sudow w !sudo tee % > /dev/null
+
 
 "JSON Fix.
 map <leader>jt  <Esc>:%!json_xs -f json -t json-pretty<CR>
 map <leader>xml  <Esc>:%!xmllint --format %<CR>
-"
+map <leader>upper :call FixCamel()<CR>
+
+
+
+"Omnicomplete
+set ofu=syntaxcomplete#Complete
+
+au FileType java set tags=~/.tags,./tags;
+
 "Gundo 
 nnoremap <F8> :GundoToggle<CR>
+syntax on
+"Neocomplete
+let g:neocomplcache_enable_at_startup = 1
 
+set modifiable
+let g:YUNOcommit_after = 2
+
+
+"snippets 
+"" Trigger configuration. Do not use <tab> if you use https://github.com/Valloric/YouCompleteMe.
+let g:UltiSnipsExpandTrigger="<tab>"
+let g:UltiSnipsJumpForwardTrigger="<c-b>"
+let g:UltiSnipsJumpBackwardTrigger="<c-z>"
+
+" If you want :UltiSnipsEdit to split your window.
+let g:UltiSnipsEditSplit="vertical"
+
+" NerdTree fix
+let g:NERDTreeDirArrows = 1
+let g:NERDTreeDirArrowExpandable = '▸'
+let g:NERDTreeDirArrowCollapsible = '▾'
--- setup.sh
+++ setup.sh
@@ -11,22 +11,16 @@ function sanityCheck()
     which cabextract >& /dev/null
     
     if [ "$?" -ne 0 ] ; then
-        echo "missing cabextract, please install it via apt-get install cabextract or yum install cabextract"
-        exit 1;
-    fi    
+    	echo "missing cabextract, please install it via apt-get install cabextract or yum install cabextract"
+    	exit 1;
+    fi	
     
     which curl >& /dev/null
     
     if [ "$?" -ne 0 ] ; then
-        echo "missing curl, please install it via apt-get install curl or yum install curl"
-        exit 1;
-    fi    
-}
-
-function fetchVistaFonts()
-{
-    (cd $HOME; $PROJECT_DIR/vistafonts-installer )
-    
+    	echo "missing curl, please install it via apt-get install curl or yum install curl"
+    	exit 1;
+    fi	
 }
 
 function doGit() 
@@ -36,59 +30,52 @@ function doGit()
 
 }
 
-function install_fonts()
-{
-    
-    mkdir $HOME/.fonts
-    fetchVistaFonts
-    echo "Installing liberation fonts..."
-    mkdir -pv $HOME/.fonts/liberation
-    cp fonts/liberation/*.ttf $HOME/.fonts/liberation
-    echo "Installing ubuntu fonts..."
-    mkdir -pv $HOME/.fonts/ubuntu
-    cp fonts/ubuntu/*.ttf $HOME/.fonts/ubuntu
-    echo "update font cache"
-    fc-cache
-    
-    
-}
-
 function clean()
 {
 	rm -friv $HOME/{.zshrc,.aliases,.vim,.vimrc}
 	cd $HOME
-	ln -s $PROJECT_DIR/.zshrc
+	ln -s $PROJECT_DIR/zshrc.zplug $HOME/.zshrc
 	ln -s $PROJECT_DIR/.vim
 	ln -s $PROJECT_DIR/.vimrc
 	ln -s $PROJECT_DIR/.zsh_local
-	ln -s $PROJECT_DIR/zsh_functions/generic_functions.zsh  $PROJECT_DIR/.zsh_local/
-	ln -s $PROJECT_DIR/zsh_functions/aliases.zsh $PROJECT_DIR/.zsh_local/
-	ln -s $PROJECT_DIR/zsh_functions/history.zsh $PROJECT_DIR/.zsh_local/
-	ln -s $PROJECT_DIR/zsh_functions/hotkeys.zsh $PROJECT_DIR/.zsh_local/
-	ln -s $PROJECT_DIR/zsh_functions/maven.zsh $PROJECT_DIR/.zsh_local/
+	ln -s $PROJECT_DIR/zplug $HOME/.zplug
+	ln -s $PROJECT_DIR/zsh_functions/generic_functions.zsh  $HOME/.zsh_local/
+	ln -s $PROJECT_DIR/zsh_functions/aliases.zsh $HOME/.zsh_local/
+	ln -s $PROJECT_DIR/zsh_functions/history.zsh $HOME/.zsh_local/
+	ln -s $PROJECT_DIR/zsh_functions/hotkeys.zsh $HOME/.zsh_local/
+	ln -s $PROJECT_DIR/zsh_functions/maven.zsh $HOME/.zsh_local/
+	ln -s $PROJECT_DIR/zsh_functions/gitstatus.py $HOME/.zsh_local/
+	ln -s $PROJECT_DIR/zsh_functions/zshrc.sh $HOME/.zsh_local/
+    ln -s $PROJECT_DIR/zsh_functions/neustar.zsh $PROJECT_DIR/.zsh_local/
 }		
 
 function init()
 {
-    clean
-    echo "Creating standard dirs"
-    for i in projects seeds torrent local workspace iso iso/linux iso/msft iso/mac; do 
-        if [ ! -e ${i} ]; then
+	clean
+	echo "Creating standard dirs"
+	for i in projects seeds torrent local workspace iso iso/linux iso/msft iso/mac; do 
+    	if [ ! -e ${i} ]; then
            mkdir $HOME/$i >& /dev/null
-        fi 
-    done
-    
+		fi 
+	done
+	
+}
+
+function fetchGitHubUtils()
+{
+	curl -s http://github.com/github/upload/raw/master/upload.rb > $PROJECT_DIR/git/github_upload
+	chmod a+x $PROJECT_DIR/git/github_upload
 }
 
 function message()
 {
-    
+	
      echo "Standard coding projects go in $HOME/projects"
      echo "eclipse code goes in $HOME/workspace"
-     echo "any $HOME install goes into $HOME/local and binary is symlinked in $PROJECT_DIR/private"
+     echo "any $HOME install goes into $HOME/local and binary is symlinked in $HOME/bin/private"
      echo "seeds/torrents are self explantory if used"
      echo "most of my system end up with one or two isos.. which go in the iso folder, optionally sorted by OS"
-    
+	
 }
 
 
@@ -107,16 +94,6 @@ fi
 sanityCheck
 init
 
-
-echo -n  'Do you wish to install project fonts to $HOME? (y|n) '
-read REPLY
-
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
-    install_fonts
-    message
-    rm -v $HOME/PowerPointViewer.exe
-fi
 
 echo -n  'Do you wish to install git customizations into $HOME? (y|n) '
 read REPLY
